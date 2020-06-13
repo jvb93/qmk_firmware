afd8e6768ca3616e753f5a0c3a6a94031b1ef9a2
jvb93 <1976577+jvb93@users.noreply.github.com>
2020-06-12 22:22:35 -0700

    macros for releasing RDP, switching between virtual desktops

--- keyboards/keebio/bdn9/keymaps/jvb93/keymap.c
+++ keyboards/keebio/bdn9/keymaps/jvb93/keymap.c
@@ -20,34 +20,41 @@ enum encoder_names {
   _RIGHT,
 };
 
+enum custom_keycodes {
+    RELEASE = SAFE_RANGE,
+    GO_RIGHT_DESKTOP,
+    GO_LEFT_DESKTOP,
+    RESET_ZOOM
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
-        | Knob 1: Vol Dn/Up |            | Knob 2: Vol Dn/Up |
-        | Press: Mute       | Home       | Press: Mute       |
-        | Previous Track    | Play/Pause | Next Track        |
-        | Hold: Layer 2     | Calculator | RGB Mode          |
+        | Knob 1: Zoom in/out     |            | Knob 2: Vol Dn/Up |
+        | Press: Reset Zoom Level | Home       | Press: Mute       |
+        | Previous Track          | Play/Pause | Next Track        |
+        | Hold: Layer 2           | Calculator | RGB Mode          |
      */
     [0] = LAYOUT(
-        KC_MUTE, KC_HOME, KC_MUTE, 
+        RESET_ZOOM, KC_HOME, KC_MUTE, 
         KC_MPRV, KC_MPLY, KC_MNXT, 
         MO(1), KC_CALC, RGB_MOD),
     /*
-        | RESET          | N/A  | Media Stop |
-        | Held: Layer 2  | Home | RGB Mode   |
-        | Held: Layer 2  | End  | Media Next |
+        | RESET          | N/A                              | Media Stop                        |
+        | Held: Layer 2  | Release from RDP                 | RGB Mode                          |
+        | Held: Layer 2  | Go Left Through Virtual Desktops | Go Right Through Virtual Desktops |
      */
     [1] = LAYOUT(
         RESET, BL_STEP, KC_STOP,
-        KC_TRNS, KC_HOME, RGB_MOD, 
-        KC_TRNS, KC_END, KC_MNXT),
+        KC_TRNS, RELEASE, RGB_MOD, 
+        KC_TRNS, GO_LEFT_DESKTOP, GO_RIGHT_DESKTOP),
 };
 
 void encoder_update_user(uint8_t index, bool clockwise) {
     if (index == _LEFT) {
         if (clockwise) {
-            tap_code(KC_VOLU);
+            SEND_STRING(SS_LCTL(SS_TAP(X_EQUAL)));
         } else {
-            tap_code(KC_VOLD);
+            SEND_STRING(SS_LCTL(SS_TAP(X_MINUS)));
         }
     }
     else if (index == _RIGHT) {
@@ -58,3 +65,29 @@ void encoder_update_user(uint8_t index, bool clockwise) {
         }
     }
 }
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case RELEASE:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LCTL(SS_LALT(SS_TAP(X_HOME))));
+            }
+            break;
+        case GO_RIGHT_DESKTOP:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LCTL(SS_RWIN(SS_TAP(X_RIGHT))));
+            }
+            break;
+        case GO_LEFT_DESKTOP:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LCTL(SS_RWIN(SS_TAP(X_LEFT))));
+            }
+            break;
+        case RESET_ZOOM:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LCTL(SS_TAP(X_0)));
+            }
+            break;
+    }
+    return true;
+};
